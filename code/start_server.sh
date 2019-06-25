14a64d07f7bad88ae6d773c5838f4274127cb1c5
Kyle Barron <barronk@mit.edu>
2019-06-25 15:38:33 -0700

    Fix path to data

--- code/start_server.sh
+++ code/start_server.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-docker run -d --rm -it -v $(pwd):/data -p 8080:80 klokantech/tileserver-gl --mbtiles data/snow_depth.mbtiles --verbose
+docker run -d --rm -it -v $(pwd):/data -p 8080:80 klokantech/tileserver-gl --mbtiles ../data/snow_depth.mbtiles --verbose
